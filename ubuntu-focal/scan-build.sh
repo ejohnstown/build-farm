006384d5c829d9a48a27a9d5989c6f001cc23a69
John Safranek <john@wolfssl.com>
2020-11-20 16:50:55 -0800

    remove the scripts, they end up in the images, don't want that

--- ubuntu-focal/scan-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm farm:focal bash -c '\
-    git clone --single-branch --depth 1 https://github.com/wolfSSL/wolfssl.git && \
-    cd wolfssl && \
-    ./autogen.sh && 
-    scan-build ./configure --disable-jobserver --enable-opensslextra --disable-memory && 
-    scan-build make'
--- ubuntu-xenial/build-wolfssl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm farm:xenial bash -c '\
-    git clone --single-branch --depth 1 https://github.com/wolfSSL/wolfssl.git && \
-    cd wolfssl && ./autogen.sh && ./configure --disable-jobserver && make check'
